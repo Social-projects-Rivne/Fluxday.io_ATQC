c848a7e448f12c1fb03f95c541193392ab32a8de
Serhii Shykor <serhiishykor@gmail.com>
2018-10-23 19:36:17 +0300

    Added POGeneral and POLogin Page Objects

---
+++ FluxDayAutomation/PageObjects/POLogin.cs
@@ -0,0 +1,15 @@
+﻿// Page Object Model for Login page
+
+namespace FluxDayAutomation.PageObjects
+{
+    class POLogin : POGeneral
+    {
+        public POLogin() : base("https://app.fluxday.io/")
+        {
+            base.AddElement("user_email", "inputbox");
+            base.AddElement("user_password", "inputbox");
+            base.AddElement("user_remember_me", "checkbox");
+            base.AddElementByClass("btn-login", "button");
+        }
+    }
+}
